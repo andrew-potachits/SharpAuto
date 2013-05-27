7caa8407a945e1139098b74959dbda3831c40164
andrew-potachits <andrew.potachits@gmail.com>
2013-05-28 01:46:02 +0300

    [~] initial version

---
+++ SpecFlowTtrial/App/TestApp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+
+namespace SpecFlowTtrial.App
+{
+
+    public class TestApp
+    {
+        private readonly RemoteWebDriver _webDriver;
+
+        public TestApp()
+        {
+            _webDriver = new FirefoxDriver();
+        }
+
+        public bool IsLoggedIn
+        {
+            get { return _webDriver.FindElements(By.ClassName("loggedInView")).Count > 0; }
+        }
+
+        public void Logoff()
+        {
+            var logoutLink = _webDriver.FindElements(By.PartialLinkText("Log Out"));
+            if (logoutLink.Count == 0)
+                return;
+            logoutLink[0].Click();
+        }
+
+        public void Navigate(string url)
+        {
+            _webDriver.Url = url;
+            _webDriver.Navigate();
+        }
+
+        public void Login(string userName, string password)
+        {
+            _webDriver.FindElement(By.Id("ShowLogin")).Click();
+
+            var login = _webDriver.FindElement(By.Id("ctl00_ucloginPopup_txtEmail"));
+            if (login == null)
+                throw new InvalidOperationException("Can't find ctl00_ucloginPopup_txtEmail");
+            login.SendKeys(userName);
+
+            var pass = _webDriver.FindElement(By.Id("ctl00_ucloginPopup_txtPwd"));
+            if (pass == null)
+                throw new InvalidOperationException("Can't find ctl00_ucloginPopup_txtPwd");
+            pass.SendKeys(password);
+
+            var loginButton = _webDriver.FindElement(By.Id("ctl00_ucloginPopup_btnLogin"));
+            if (loginButton == null) 
+                throw new InvalidOperationException("Can't find ctl00_ucloginPopup_btnLogin");
+            loginButton.Click();
+        }
+    }
+}
---
+++ SpecFlowTtrial/Features/SpecFlowFeature1.feature
@@ -0,0 +1,17 @@
+﻿Feature: TestFeature
+
+	
+Background: open homepage
+Scenario: go to homepage
+	Given navigate to http://www.fins.com/
+
+Scenario: login OK
+	Given I am not logged in
+	When I enter paa@intetics.com and Newbuild14
+	Then Login Succeeded
+
+Scenario: login failure
+	Given I am not logged in
+	When I enter paa@intetics.com and incorrectPassword
+	Then Login Failed
+
